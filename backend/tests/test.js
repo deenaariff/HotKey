b921e75fb4d68aaa442bcc8e355369829199bc7b
daariff <aariff.deen@gmail.com>
2016-02-13 00:52:57 -0800

    added dashboard

--- backend/tests/test.js
+++ backend/tests/test.js
@@ -1,34 +1,22 @@
-var test = require('./test.json'); 
+var services = require('../mock-services.json');
+var users = require('../mock-users.json'); 
 
 var print = function (data) {
-	console.log("Loaded Rescource..");
 
-	var collection1 = data[0][0];
-	var collection2 = data[0][1];
-
-
-	console.log("Loaded Collections..");
-
-	//var users = collection1[0];
-	//var services = collection1[1];
-
-
-	// Print Users
-	//for(var i = 0; i < users.length(); i++) {
-		//console.log(JSON.stringify(users[0]));
-	//}
+	for(var i = 0; i < users.length(); i++) {
+		console.log(JSON.stringify(users[i]));
+	}
 
 	console.log("\n");
 
-	// Print Services
-	//for(var i = 0; i < services.length(); i++) {
-		//console.log(JSON.stringify(users[0]));
-	//}
+	for(var i = 0; i < services.length(); i++) {
+		console.log(JSON.stringify(users[i]));
+	}
 };
 
 var execute = function () {
 	console.log("Started");
-	print(test);
+	print();
 };
 
 execute();
