dcbbcde2c3f4439fa0a9e1e5219bbf17a07f158a
Norman Tasfi <ntasfi@gmail.com>
2016-02-13 22:40:54 -0800

    sudoku run leeego

--- frontend/view/autofill.js
+++ frontend/view/autofill.js
@@ -1,16 +1,4 @@
-var port = chrome.runtime.connect({name: "autofill"});
-port.onMessage.addListener(function(message, sender) {
-	if(message.greeting == "hello") {
-		alert(message.greeting);
-		chrome.extension.getBackgroundPage().alert("hi");
-	}
-});
-
-
-$( document ).ready(function(){
-	alert("testing outside");
-	chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
-		alert("testing");
-		sendResponse("response....");
-	});
+window.addEventListener("storage", function(event) {
+	alert("HELLO STORAGE");
+	//login with data here...
 });
--- frontend/view/background.js
+++ frontend/view/background.js
@@ -1,19 +1,12 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        switch (request.directive) {
-        case "popup-click":
-            // execute the content script
-            chrome.tabs.executeScript(null, { // defaults to the current tab
-                file: "contentscript.js", // script to inject into page and run in sandbox
-                allFrames: true // This injects script into iframes in the page and doesn't work before 4.0.266.0.
-            });
-            sendResponse({}); // sending back empty response to sender
-            break;
-        default:
-            // helps debug when request directive doesn't match
-            alert("Unmatched request of '" + request + "' from script to background.js from " + sender);
-        }
-    }
-);
+window.addEventListener("storage", function(event) {
+//delay for a second?
+var i = 0;
+while (i < 100000) {
+	i++;
+}
+data_ = localStorage.getItem("data")
+data = localStorage.setItem("data_", data_)
+//modify the storage slightly?
+//and the contentjs will get triggered now!
 
-// chrome.extension.getBackgroundPage().alert("hi");
+});
