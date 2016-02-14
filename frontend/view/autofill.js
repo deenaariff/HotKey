cef44cef2598154108c872b7b6b09843e99f0247
daariff <aariff.deen@gmail.com>
2016-02-13 22:37:45 -0800

    changes

--- frontend/view/autofill.js
+++ frontend/view/autofill.js
@@ -1,8 +1,8 @@
 $( document ).ready(function(){
-	alert("testing outside");
-	chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
-		alert("testing");
-		sendResponse("response....");
+	
+	$.get("https://localhost:3000/getData", function(data){
+  		alert("Data: " + data);
 	});
+
 });
 
--- frontend/view/popup.js
+++ frontend/view/popup.js
@@ -8,13 +8,28 @@ function changeHostName(name) {
   document.getElementById('host').innerHTML=name;
 }
 
+function pingContent(tab) {
+	chrome.runtime.sendMessage(tabs[0].id,{cmd: "test"}, function(res){
+		alert(res.sure)
+	});
+}
+
 $( document ).ready(function() {
 	changeHostName("Ben Shukman");
-	document.getElementById("button").addEventListener('click', function() {
+	var name = "Ben Shukman";
+
+	// AFTER GET RESPONSE
+	function newdoc () {
 		chrome.tabs.create({url:this.getAttribute('href')}, function(tab) {
-			chrome.runtime.sendMessage({cmd: "test"}, function(res){
-				alert(res.sure)
-			})
+			console.log("Sent");
+		});
+	};
+
+	document.getElementById("button").addEventListener('click', function() {
+		$.post("http://localhost:3000/storeData", name, function(data) {
+  			newdoc();
 		});
 	});
+
+
 });
